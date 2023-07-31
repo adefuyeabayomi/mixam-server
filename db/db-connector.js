451c11e152077f80096d30f30962c3ad9a6c3aec
adefuyeabayomi <63407534+adefuyeabayomi@users.noreply.github.com>
2023-07-31 12:53:35 +0100

    updated mongodb

--- db/db-connector.js
+++ db/db-connector.js
@@ -1,5 +1,5 @@
 const fastifyPlugin = require('fastify-plugin')
-async function dbConnector (fastify, options) {  fastify.register(require('fastify-mongodb'),
+async function dbConnector (fastify, options) {  fastify.register(require('@fastify/mongodb'),
  {    url: process.env.DB_URL, forceClose: true }) }
 // Wrapping a plugin function with fastify-plugin exposes the decorators    
 // and hooks, declared inside the plugin to the parent scope. 
